3f1028f4b55e4478c6996aa9ec39e380bb5f5f47
Outvi V <git@outv.im>
2019-12-25 16:34:02 +0800

    Remove extra routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/tags',
     name: 'tagsearc',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
